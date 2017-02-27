e04cfd8301f91f91b962018d90de87438f329de5
eneko0513 <eneko0513@users.noreply.github.com>
2017-02-27 22:39:10 +0900

    Update test.js

--- test.js
+++ test.js
@@ -32,8 +32,8 @@ javascript: (function(f, dd) {
 	});
 	
 	var comment_limit = 75;
-	var versions = "0.8"; 
-	var uptext = '\n75文字突破機能の実装\n(※悪用対策として184コメント時は不可,各自使い方は自己責任)';
+	var versions = "0.9"; 
+	var uptext = '\n先頭行と最終行の文字数カウントを追加(※末尾の<br>はカウントされません)\nコメント文字数オーバー時の投下の際に警告表示を追加';
 	$("div.NicoenqueteNotificationContainer").before("<div id='scriptdiv'><button id='auto_insert'>AUTO-ALL</button><button id='single_insert'>SINGLE</button><button id='clear'>CLEAR</button><span style='margin-right: 6em;'></span><button id='convert'>Dohmo->SINGLE</button><span style='margin-right: 10em;'></span><button id='version'>ver:" + versions +"(更新内容)</button><span style='margin-right: 6em;'></span><form><label for='id_aaa' >184投下</label><input id='check_184' type='checkbox' value='check_184'><span style='margin-right: 2em;'></span><label for='id_aaa' >下から投下</label><input id='check_return' type='checkbox' value='check_return'><span style='margin-right: 2em;'></span><label for='id_aaa' >1024投下</label><input id='check_over75' type='checkbox' value='check_over75'><span style='margin-right: 10em;'></span><label id='first_line_length' >先頭行文字数:0</label><span style='margin-right: 2em;'></span><label id='last_line_length' >最終行文字数:0</label></form><br><textarea id='script_text_area' style='margin: 0px; width: 641px; height: 122px;'></textarea></div>");
 
 	function button_disabled_change(flag) {
