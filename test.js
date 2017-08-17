04b2fd9eec57e2a5ff55c0ee52090f24fafa01b4
eneko0513 <eneko0513@users.noreply.github.com>
2017-08-17 22:37:21 +0900

    Update test.js

--- test.js
+++ test.js
@@ -12,21 +12,37 @@ javascript: (function(f, dd) {
 		var text = $("#script_text_area").val().replace(/\r\n|\r/g, "\n");
 		var lines = text.split('\n');
 		text = lines[0];
-		if (text.match(/^\[(.+?)\](.*)/) != null) {
-			var posetSet = setInterval(function() {
-				if ($("#script_text_area").val() == "") clearInterval(posetSet);
-				button_disabled_change(false);
-			}, 6000);
-			setCommandMment();
-			button_disabled_change(true);
-			var posetSet = setInterval(function() {
+
+		if(element.classList.contains('GridCell OwnerEditMenuContainer-left') == false ){
+			if (text.match(/^\[(.+?)\](.*)/) != null) {
+				var posetSet = setInterval(function() {
+					if ($("#script_text_area").val() == "") clearInterval(posetSet);
+					button_disabled_change(false);
+				}, 6000);
 				setCommandMment();
-			}, 6000);
-		} else {
-			alert("not [COMMAND]COMMENT");
+				button_disabled_change(true);
+				var posetSet = setInterval(function() {
+					setCommandMment();
+				}, 6000);
+			} else {
+				alert("not [COMMAND]COMMENT");
+			}
+		}else{
+			//クソ適当な投コメモード対応
+			if (text.match(/^\[(.+?)\](.*)/) != null) {
+				var posetSet = setInterval(function() {
+					if ($("#script_text_area").val() == "") clearInterval(posetSet);
+					button_disabled_change(false);
+				}, 1500);
+				setCommandMment();
+				button_disabled_change(true);
+				var posetSet = setInterval(function() {
+					setCommandMment();
+				}, 1500);
+			} else {
+				alert("not [COMMAND]COMMENT");
+			}
 		}
-		
-		
 	});
 	
 	var comment_limit = 75;
