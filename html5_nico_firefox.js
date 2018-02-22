5ead0fc7dc23cd9ec98df2900154150aa4994b92
eneko0513 <eneko0513@users.noreply.github.com>
2018-02-23 01:56:58 +0900

    update内容を通知
    とりあえず良かった。

--- html5_nico_firefox.js
+++ html5_nico_firefox.js
@@ -54,8 +54,8 @@ javascript: (function(f, dd) {
 	});
 
 	var comment_limit = 75;
-	var versions = "0.94";
-	var uptext = '\n投コメ編集画面でスクリプトを起動した場合に、コメント入力欄の文字数制限を1024文字に変更する処理を追加。';
+	var versions = "1.00";
+	var uptext = '\n自動投下ができなくなっていた問題を解決';
 	$("div.NicoenqueteNotificationContainer").before("<div id='scriptdiv'><button id='auto_insert'>AUTO-ALL</button><button id='single_insert'>SINGLE</button><button id='clear'>CLEAR</button><span style='margin-right: 6em;'></span><button id='convert'>Dohmo->SINGLE</button><button id='convert_json'>エディタ1行変換</button><span style='margin-right: 5em;'></span><button id='version'>ver:" + versions +"(更新内容)</button><span style='margin-right: 6em;'></span><form><label for='id_aaa' >184投下</label><input id='check_184' type='checkbox' value='check_184'><span style='margin-right: 2em;'></span><label for='id_aaa' >下から投下</label><input id='check_return' type='checkbox' value='check_return'><span style='margin-right: 2em;'></span><label for='id_aaa' >1024投下</label><input id='check_over75' type='checkbox' value='check_over75'><span style='margin-right: 2em;'></span><label for='id_aaa' >pattisier付与</label><input id='patissier' type='checkbox' value='patissier'><span style='margin-right: 3em;'></span><label id='first_line_length' >先頭行文字数:0</label><span style='margin-right: 2em;'></span><label id='last_line_length' >最終行文字数:0</label></form><br><textarea id='script_text_area' style='margin: 0px; width: 641px; height: 122px;'></textarea><br><label for='id_bbb' >↓エディタのjson形式を1コメント1行単位置換したもの↓</label><br><textarea id='edit_json_output' style='margin: 0px; width: 641px; height: 122px;'></textarea></div>");
 
 	function button_disabled_change(flag) {
