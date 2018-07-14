4a1868839763b3d0c06da8534bdb82910aaaa112
eneko0513 <eneko0513@users.noreply.github.com>
2018-07-15 00:12:04 +0900

    cc

--- html5_nico_firefox.js
+++ html5_nico_firefox.js
@@ -156,7 +156,7 @@
 		"	</label>		" +
 		"	<label id='mode' class='tab5_label' for='tab5'><font color='#ff0000'>モード： </font>" +
 		"	</label>		" +
-		"	<label id='tab4_2' class='tab4_label' for='tab4'>Version:0.7" +
+		"	<label id='tab4_2' class='tab4_label' for='tab4'>Version:0.9" +
 		"	</label>		" +
 		"  </div>		" +
 		"  <div class='panel_area'>			" +
