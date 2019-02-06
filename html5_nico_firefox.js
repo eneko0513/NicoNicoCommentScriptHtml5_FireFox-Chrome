b47ad77d09ffa52e5e195f920292a104d2103226
eneko0513 <ion.n@hotmail.co.jp>
2019-02-07 00:36:04 +0900

    b

--- html5_nico_firefox.js
+++ html5_nico_firefox.js
@@ -219,6 +219,8 @@
 		"	  <p>" +
 		"		<input id='textColorChange' class='ActionButton TagEnterEditingButton TagContainer - editButton' type='button' /'>" +
 		"		<label for='textColorChange' class='label'>選択レイヤー色変更</label>" +
+		"		<input id='layerNameChange' class='ActionButton TagEnterEditingButton TagContainer - editButton' type='button' /'>" +
+		"		<label for='layerNameChange' class='label'>レイヤー名前変更</label>" +
 		"		<input id='layerVisible' class='ActionButton TagEnterEditingButton TagContainer - editButton' type='button' /'>" +
 		"		<label for='layerVisible' class='label'>選択レイヤー非表示</label>" +
 		"		<input id='layerAllVisible' class='ActionButton TagEnterEditingButton TagContainer - editButton' type='button' /'>" +
