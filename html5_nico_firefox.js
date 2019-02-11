8fc7ce4be7e0f6ff8a806399a0efbafd142e54fc
eneko0513 <ion.n@hotmail.co.jp>
2019-02-12 02:24:17 +0900

    Update html5_nico_firefox.js

--- html5_nico_firefox.js
+++ html5_nico_firefox.js
@@ -1856,7 +1856,7 @@ javascript: (function (f, dd) {
 	$('#layerOutputAll').click(function () {
 		count = 0;
 		AllClick = true;
-		for (var i = 0; i < $('#myTrcSel2').length; i++) {
+		for (var i = 0; i <= $('#myTrcSel2').length; i++) {
 			$('#myTrcSel2')[i].selected = true;
 			$('#layerOutput').click();
 		}
