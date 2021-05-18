fd98b812a3b25780e86e07a0cd17660eaa7621ae
eneko0513 <eneko0513@users.noreply.github.com>
2021-05-18 23:38:48 +0900

    Update html5_nico_firefox.js

--- html5_nico_firefox.js
+++ html5_nico_firefox.js
@@ -195,7 +195,7 @@
 		"	</label>		" +
 		"	<label id='mode' class='tab5_label' for='tab5'>モード： " +
 		"	</label>		" +
-		"	<label id='tab4_2' class='tab4_label' for='tab4'>Version:2.0.0" +
+		"	<label id='tab4_2' class='tab4_label' for='tab4'>Version:2.1.0" +
 		"	</label>		" +
 		"  </div>		" +
 		"  <div class='panel_area'>			" +
